8d03d174fafc1a7d4b8834152649e632abdb8f76
Detective-Coder <aaronfrankenfield@gmail.com>
2021-02-26 15:53:36 -0500

    printing animals customers with react

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,10 +1,12 @@
 import React from "react"
 import { Route } from "react-router-dom"
 import { Home } from "./Home"
-import { AnimalCard } from "./animal/AnimalCard"
-import { CustomerCard } from "./customer/Customer"
 import { EmployeeCard } from "./employee/Employee"
 import { LocationCard } from "./location/Location"
+import { AnimalProvider } from "./animal/AnimalProvider"
+import { AnimalList } from "./animal/AnimalList"
+import {CustomerList} from "./customer/CustomerList"
+import {CustomerProvider} from "./customer/CustomerProvider"
 
 export const ApplicationViews = () => {
     return (
@@ -14,15 +16,17 @@ export const ApplicationViews = () => {
                 <Home />
             </Route>
 
-            {/* Render the animal list when http://localhost:3000/animals */}
-            <Route path="/animals">
-                <AnimalCard />
-            </Route>
+            <AnimalProvider>
+              <Route exact path="/animals">
+                  <AnimalList />
+              </Route>
+            </AnimalProvider>
 
-            {/* Render the customer list when http://localhost:3000/customers */}
-            <Route path="/customers">
-                <CustomerCard />
-            </Route>
+            <CustomerProvider>
+              <Route path="/customers">
+                  <CustomerList />
+              </Route>
+            </CustomerProvider>
 
             {/* Render the employee list when http://localhost:3000/employees */}
             <Route path="/employees">
